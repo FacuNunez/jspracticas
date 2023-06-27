058e4408dbc785fd46bfd07064e5848aaf528636
Facundo Nuñez <fnunez1981@gmail.com>
2023-06-27 20:12:52 -0300

    git commit m "after4"

--- after4/index.js
+++ after4/index.js
@@ -1,5 +1,9 @@
 let div = document.getElementById("div");
+let carritoLleno = document.getElementById("click");
+let inputAfter = document.getElementById("inputAfter");
+let botonBuscar = document.getElementById("botonInput");
 
+//funcion contracuctora
 function Producto(id, nombre, precio, imagen) {
   this.id = id;
   this.name = nombre;
@@ -8,7 +12,7 @@ function Producto(id, nombre, precio, imagen) {
 }
 
 const tienda = [];
-
+//pusheo de productos
 tienda.push(
   new Producto(
     1,
@@ -26,6 +30,10 @@ tienda.push(
   )
 );
 
+const carrito = [];
+
+//DOM
+//renderizado del producto
 tienda.forEach((producto) => {
   let productoRenderizado = document.createElement("div");
   productoRenderizado.innerHTML = `
@@ -35,9 +43,45 @@ tienda.forEach((producto) => {
                 <h5 class="card-title">${producto.name}</h5>
                 <p class="card-text">$ ${producto.price}</p>
             </div>
-            <button class="boton">COMPRAR</button>
+            <button id=${producto.id}>COMPRAR</button>
     </div>
     `;
 
   div.append(productoRenderizado);
+  const boton = document.getElementById(producto.id);
+  boton.addEventListener("click", () => comprarProducto(producto));
 });
+
+//EVENTO
+//el evento va a funcionar sobre un nodo, si dos nodos tiene el mismo id, va a funcionar sobre uno solo
+const comprarProducto = (producto) => {
+  let productoExiste = carrito.find((item) => item.id === producto.id);
+  if (productoExiste !== undefined) {
+    productoExiste.precio = productoExiste.precio + producto.price;
+    productoExiste.cantidad = productoExiste.cantidad + 1;
+  } else {
+    carrito.push({
+      id: producto.id,
+      nombre: producto.name,
+      precio: producto.price,
+      imagen: producto.image,
+      cantidad: 1,
+    });
+  }
+};
+
+const buscarProducto = (string) => {
+  console.log(string);
+  let productoBuscado = tienda.find(producto =>
+    producto.name.includes(string)
+  );
+  console.log(productoBuscado);
+  inputAfter.value = " ";
+};//
+
+carritoLleno.addEventListener("click", () => console.log(carrito));
+
+//EVENTOS INPUT
+//BUSCADOR
+botonBuscar.addEventListener("click", () => buscarProducto(inputAfter.value));
+
